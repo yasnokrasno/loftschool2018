8fcf73b1bec0bd09faf692ea26529c3076bf8a99
yasnokrasno <gvladislavn@yandex.ru>
2018-11-25 16:09:47 +0300

    Homework 2 - refactor generate-mess.js

--- README.md
+++ README.md
@@ -1,27 +1,37 @@
-#LoftSchool Homeworks
+# LoftSchool Homeworks
 
-##01. Librarian
+## Summary
 
-Simple files sorting based on native NodeJS fs callbacks.
-Trying to struggle with "callback hell"
-without of Promises, Generators and Async/Await features.
+1. [Librarian](#1-librarian) - copying files from source directory with random nesting to new sorted directory. 
+Includes only two dependencies: [fs](https://nodejs.org/api/fs.html) and [path](https://nodejs.org/api/path.html)
+2. [Librarian2](#2-librarian2) - refactored version of Librarian with Promises & Async functions
+
+## 1. Librarian
+
+Simple files sorting based on native NodeJS [fs](https://nodejs.org/api/fs.html) callbacks.
+Trying to struggle with "callback hell" without of 
+[Promises](https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Promise), 
+[Generators](https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Generator) 
+and [Async functions](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/async_function) features.
 
 All files from given directory will be moved to new target directory 
 in subdirectories, corresponding to the first letter of the file name.
 
-Quickstart:
+#### Quickstart:
 
-``npm run hw-01-init`` then
-``npm run hw-01``
+```
+npm run hw-01-init
+npm run hw-01
+```
 
 Task will be completed in default directories.
 If you want to change target or source directories you can run script directly:
 
-``cd hw-01/scripts``
-
-``node generate-mess.js -dir ../mess -fill 10 -dep 5``
-
-``node librarian.js -from ../mess -to ../result``
+```
+cd hw-01/scripts
+node generate-mess.js -dir ../mess -fill 10 -dep 5
+node librarian.js -from ../mess -to ../result
+```
 
 Argument names are prefixed by `-` and stands before corresponding value. 
 Flags (without of value) are prefixed by `--`.
@@ -38,3 +48,22 @@ __librarian.js__
 * `-from` - (_string_) source directory path. Default is: `"../mess"`
 * `-to` - (_string_) target directory path. Default is: `"../result"`
 * `--delsrc` - delete source directory. 
+
+## 2. Librarian2
+
+Librarian2 is equal to Librarian from first homework, but using Promises & Async functions.
+
+#### Quickstart:
+
+```
+npm run hw-02-init
+npm run hw-02
+```
+
+Arguments are parsed by [Minimist](https://www.npmjs.com/package/minimist), 
+so format will be the next:
+
+- `node generate-mess2.js --dir=../mess --fill=10 --dep=5`
+- `node librarian2.js --from=../mess --to=../result`
+
+`generate-mess2.js` uses Promises and `librarian2.js` uses Async functions.
---
+++ hw-02/scripts/generate-mess2.js
@@ -0,0 +1,91 @@
+const fs = require('fs');
+const path = require('path');
+const parseArgs = require('minimist');
+
+const nameGen = require('../../hw-01/scripts/helpers/namegen');
+
+const NAME_LEN = 5; // name length for mock files in mess directory
+
+const rawArgs = parseArgs(process.argv.slice(2));
+const settings = Object.assign({
+  dep: 3, // default directories depth
+  fill: 10, // default items quantity for each directory
+  dir: '../mess', // default mess directory path
+}, rawArgs);
+settings.dir = path.resolve(settings.dir);
+
+if (fs.existsSync(settings.dir)) {
+  console.error('Librarian ERROR: path already exists: ', settings.dir);
+  process.exit(3);
+}
+
+fs.mkdir(settings.dir, { recursive: true }, function () {
+  generateDirectory(settings.dir, settings.fill, settings.dep).then(function () {
+    console.log('Librarian: Mess generation is complete. See: ', settings.dir);
+  });
+});
+
+/**
+ * Recursive function for mess generation. It uses Math.random() to define
+ * whether to create a directory or a file when generating target directory content.
+ * @param dirPath
+ * @param itemsCount
+ * @param remainingDepth - need to handle recursion depth
+ */
+function generateDirectory (dirPath, itemsCount = 10, remainingDepth = 3) {
+  let dirCreationChance = remainingDepth > 1 ? 0.8 : 1; // random chance to fill target directory by subdirectories
+  remainingDepth -= 1;
+  const promisesArr = Array(itemsCount);
+
+  for (let i = 0; i < itemsCount; i++) {
+    const willMakeDir = Math.random() > dirCreationChance;
+    const makeItemFunc = willMakeDir ? makeDir : makeFile;
+    promisesArr.push(new Promise(function (resolve) {
+      makeItemFunc(dirPath).then(function (createdItemPath) {
+        if (!createdItemPath) resolve(false);
+        if (willMakeDir) {
+          generateDirectory(createdItemPath, itemsCount, remainingDepth).then(function (result) {
+            resolve(result);
+          }, () => {
+            resolve(false);
+          });
+        } else {
+          resolve(createdItemPath);
+        }
+      }, () => {
+        resolve(false);
+      });
+    }));
+  }
+  return Promise.all(promisesArr);
+}
+
+/**
+ * Create one mock file in given directory path.
+ * @param targetDirPath
+ * @returns {Promise<any>}
+ */
+function makeFile (targetDirPath) {
+  return new Promise(function (resolve) {
+    let filePath = path.join(targetDirPath, nameGen.generateFileName(NAME_LEN));
+    fs.appendFile(filePath, '// test file!!!', 'utf8', function (error) {
+      if (error) resolve(false);
+      resolve(filePath);
+    });
+  });
+}
+
+/**
+ * Create one empty directory in given target directory.
+ * @param targetDirPath
+ * @returns {Promise<any>}
+ */
+function makeDir (targetDirPath) {
+  return new Promise(function (resolve) {
+    let dirPath = path.join(targetDirPath, nameGen.generateName(NAME_LEN));
+    fs.mkdir(dirPath, { recursive: true }, function (error) {
+      if (error) resolve(false);
+      resolve(dirPath);
+    });
+  });
+}
